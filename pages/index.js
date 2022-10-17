73ea5897a7fde38bb1a4db159b6215e8e9903b9b
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-17 19:41:42 +0200

    add tailwind conf

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default function Home() {
 	return (
 		<di className='bg-red-300'>
+			test
 			<Main className=' w-2 bg-red-400'>Test</Main>
 		</di>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				// Simple 16 column grid
+				// 3: 'repeat(3, minmax(0, 1fr))',
+				3: '1fr, 3fr, minmax(0, 3fr)',
+				sm3: '1fr, 1fr, 1fr',
+
+				// Complex site-specific column configuration
+				footer: '200px minmax(900px, 1fr) 100px',
+			},
+			colors: {
+				'primary-color': 'var(--primary-color)',
+				'secondary-color': 'var(--secondary-color)',
+			},
+		},
 	},
 	plugins: [],
 };
