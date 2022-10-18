e143c264a670ccbe658f2b4a4582199f478b586a
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-18 10:32:17 +0200

    add Layout.jsx

---
+++ components/Layout.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Layout() {
+	return (
+		<div>
+			<Head>
+				<title>Tonstudio-Kleinanzeigen</title>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+			</Head>
+		</div>
+	);
+}
