90ac28e24222952e05bc37a6461a4f5822affd2c
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-19 15:16:24 +0200

    add react-date-range

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -7,7 +7,7 @@ function SearchBar() {
 	return (
 		<div>
 			<form className='flex  flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 shadow-sm md:shadow-lg'>
-				<MagnifyingGlassIcon className='h-6 w-6 flex-shrink-0 cursor-pointer rounded-full bg-black/30 p-1 text-white' />
+				<MagnifyingGlassIcon className='h-6 w-6 shrink-0 cursor-pointer rounded-full bg-black/30 p-1 text-white' />
 				<input
 					className='flex-1 border-none bg-transparent outline-none'
 					type='text'
