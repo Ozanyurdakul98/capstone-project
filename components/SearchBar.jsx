6f7563635a3dee446c7a0377b236f3deab733318
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-19 15:27:37 +0200

    add date-range component

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
+//tools
+import { DateRange } from 'react-date-range';
 //styles
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 function SearchBar() {
 	const [searchInput, setSearchInput] = useState('');
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: 'selection',
+	};
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	};
+
 	return (
 		<div>
 			<form className='flex  flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 shadow-sm md:shadow-lg'>
@@ -19,6 +37,15 @@ function SearchBar() {
 					type='submit'
 				></button>
 			</form>
+			<DateRange
+				ranges={[selectionRange]}
+				rangeColors={['#df1b1b']}
+				showMonthAndYearPickers={false}
+				onChange={handleSelect}
+				minDate={new Date()}
+				calendarFocus={'forwards'}
+				moveRangeOnFirstSelection={true}
+			/>
 		</div>
 	);
 }
