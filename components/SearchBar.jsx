73cb0c91f64051aecbf9ce6651da58a2109b4e63
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-19 15:06:40 +0200

    add HeroIcons

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 function SearchBar() {
 	return (
 		<div>
-			<form className=''>
+			<form className='flex  flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 shadow-sm md:shadow-lg'>
+				<MagnifyingGlassIcon className='h-6 w-6 flex-shrink-0 cursor-pointer rounded-full bg-black/30 p-1 text-white' />
 				<input
-					className=''
+					className='flex-1 border-none bg-transparent outline-none'
 					type='text'
 				/>
 			</form>
