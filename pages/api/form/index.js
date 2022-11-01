4a1111816823d14007483671440d52cae89e9faf
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-01 14:26:24 +0100

    add form api route handling post request

---
+++ pages/api/form/index.js
@@ -0,0 +1,33 @@
+import db from '../../lib/dbConnect';
+import StudioListing from '../../models/StudioListing';
+
+export default async function handler(req, res) {
+	const { method } = req;
+
+	await db.connect();
+
+	switch (method) {
+		case 'GET':
+			try {
+				const listing = await StudioListing.find({}); /* find all the data in our database */
+				res.status(200).json({ success: true, data: listing });
+			} catch (error) {
+				res.status(400).json({ success: false });
+			}
+			break;
+		case 'POST':
+			try {
+				const listing = await StudioListing.create(
+					req.body
+				); /* create a new model in the database */
+				res.status(201).json({ success: true, data: listing });
+			} catch (error) {
+				res.status(400).json({ success: false });
+				console.log('error', error);
+			}
+			break;
+		default:
+			res.status(400).json({ success: false });
+			break;
+	}
+}
