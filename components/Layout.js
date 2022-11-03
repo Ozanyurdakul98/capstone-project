470c811ed23cd4612aff61a5fa84f8473d9c91ad
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-03 14:36:17 +0100

    unable indenting with tabs, prettier

--- components/Layout.js
+++ components/Layout.js
@@ -6,23 +6,23 @@ import styled from 'styled-components';
 import Header from './Header';
 
 export default function Layout({ children }) {
-	return (
-		<div className='bg-site relative'>
-			<Head>
-				<title>Tonstudio-Kleinanzeigen</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<StyledHeader>
-				<Header />
-			</StyledHeader>
-			<StyledMain className='container'>{children}</StyledMain>
-			<StyledFooter>@Copyright Tonstudio-Kleinanzeigen 2022</StyledFooter>
-		</div>
-	);
+  return (
+    <div className='bg-site relative'>
+      <Head>
+        <title>Tonstudio-Kleinanzeigen</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <StyledHeader>
+        <Header />
+      </StyledHeader>
+      <StyledMain className='container'>{children}</StyledMain>
+      <StyledFooter>@Copyright Tonstudio-Kleinanzeigen 2022</StyledFooter>
+    </div>
+  );
 }
 
 const StyledHeader = styled.header``;
 const StyledMain = styled.main`
-	margin: 0 auto;
+  margin: 0 auto;
 `;
 const StyledFooter = styled.footer``;
--- models/StudioListing.js
+++ models/StudioListing.js
@@ -1,32 +1,31 @@
 import mongoose from 'mongoose';
 
 const studioListingSchema = new mongoose.Schema(
-	{
-		maxGuests: { type: Number, default: 3 },
-		listingTitle: { type: String },
-		images: {
-			type: String,
-			default: '/images/Thumbnail-Default.png',
-			set: (v) => (v === '' || undefined || null ? '/images/Thumbnail-Default.png' : v),
-		},
-		openingHours: {
-			type: String,
-			default: 'Always Available',
-		},
-		studiotype: { type: String },
-		services: { type: Array },
-		locationFeatures: { type: Array },
-		soundengineer: { type: Object },
-		studioPricing: { type: Object },
-		studioLocation: { type: String },
-	},
-	{
-		timestamps: true,
-	}
+  {
+    maxGuests: { type: Number, default: 3 },
+    listingTitle: { type: String },
+    images: {
+      type: String,
+      default: '/images/Thumbnail-Default.png',
+      set: (v) => (v === '' || undefined || null ? '/images/Thumbnail-Default.png' : v),
+    },
+    openingHours: {
+      type: String,
+      default: 'Always Available',
+    },
+    studiotype: { type: String },
+    services: { type: Array },
+    locationFeatures: { type: Array },
+    soundengineer: { type: Object },
+    studioPricing: { type: Object },
+    studioLocation: { type: String },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 // openingCustom: { type: Boolean, required: true, default: false },
 //Usermodel
-const StudioListing =
-	mongoose.models.StudioListing || mongoose.model('StudioListing', studioListingSchema);
+const StudioListing = mongoose.models.StudioListing || mongoose.model('StudioListing', studioListingSchema);
 export default StudioListing;
