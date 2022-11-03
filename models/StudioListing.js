1570c829e0cb76f0b3f7508bb6976688b8a065dd
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-03 14:02:29 +0100

    clean commented out schema

--- models/StudioListing.js
+++ models/StudioListing.js
@@ -1,35 +1,5 @@
 import mongoose from 'mongoose';
-import { nanoid } from 'nanoid';
 
-// const studioListingSchema = new mongoose.Schema(
-// 	{
-// 		id: { type: String },
-// 		title: { type: String, required: true },
-// 		studioname: { type: String, required: true },
-// 		openingOption: { type: String, required: true },
-// 		openingCustom: {
-// 			type: Object,
-// 			required: true,
-// 			default: false,
-// 		},
-// 		img: { type: String, required: true },
-// 		studiotype: { type: String, required: true },
-// 		services: { type: Array, required: true },
-// 		maxGuests: { type: Number, required: true },
-// 		soundEngineer: { type: Object, required: true },
-// 		soundEngineerPrice: { type: Object, required: false },
-// 		studioBooking: { type: Object, required: true },
-// 		description: { type: String, required: true },
-// 		locationFeatures: { type: Object, required: false },
-// 		equipment: { type: Array, required: false },
-// 		references: { type: String, required: false },
-// 		socialMedia: { type: Object, required: false },
-// 		location: { type: String, required: true },
-// 	},
-// 	{
-// 		timestamps: true,
-// 	}
-// );
 const studioListingSchema = new mongoose.Schema(
 	{
 		maxGuests: { type: Number, default: 3 },
