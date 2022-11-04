eb959173360ae53b9673ad0d168ca8661d65340d
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-04 21:48:34 +0100

    add shared session state

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,23 @@
 //pages & components
 import Layout from '../components/Layout';
+//tools
+import { SessionProvider } from 'next-auth/react';
 //styles
 import GlobalStyle from '../components/GlobalStyle';
 import '../styles/globals.css';
 import '../components/DatePicker/styles.css';
 import '../components/DatePicker/default.css';
 
-function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<GlobalStyle />
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	);
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <>
+      <GlobalStyle />
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
+  );
 }
 export default MyApp;
