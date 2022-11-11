ae9b6a693516f335f8229c69f23800d2a9916d5c
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-11 03:57:07 +0100

    make homepage banner a component

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import db from '../lib/dbConnect';
 import StudioListing from '../models/StudioListing';
 import Head from 'next/head';
-import Image from 'next/image';
 import 'react-multi-carousel/lib/styles.css';
 import { Latest10Listings } from '../components/Latest10Listings';
 import { HomepageHero } from '../components/HomepageHero';
-import Link from 'next/link';
+import { HomepageBanner } from '../components/HomepageBanner';
 
 export default function Home({ latestListings }) {
   console.log(latestListings);
@@ -19,32 +18,7 @@ export default function Home({ latestListings }) {
       </Head>
       <HomepageHero></HomepageHero>
       <Latest10Listings latestListings={latestListings}></Latest10Listings>
-      <section className='relative grid h-screen w-full'>
-        <div className='relative grid h-screen w-full '>
-          <Image
-            src='/images/Banner-homepage1.jpg'
-            objectPosition='top'
-            layout='fill'
-            objectFit='cover'
-            alt='login-image'
-          />
-        </div>
-        <div className='absolute top-1/2 left-1/2 w-80 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white/90 py-10 px-6 font-thin md:left-1/3 lg:left-1/3 lg:text-lg xl:text-xl'>
-          <p className='mb-4'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsa ipsam est quo atque quaerat veritatis
-            expedita adipisci soluta, ab veniam delectus sequi, ipsum accusantium explicabo non laudantium facere
-            commodi.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quas officiis accusamus corporis odio
-            voluptatem quidem iure earum, cupiditate voluptates consequatur. Vero architecto amet provident delectus,
-            similique consequuntur harum blanditiis incidunt distinctio libero omnis ullam sapiente cumque.
-          </p>
-          <Link href='/signup'>
-            <button className=' button mt-4 text-base font-semibold'>Become a Host</button>
-          </Link>
-        </div>
-      </section>
+      <HomepageBanner></HomepageBanner>
       {/* {session ? (
           <>
             <p>Signed in as {session.user.email}</p>
