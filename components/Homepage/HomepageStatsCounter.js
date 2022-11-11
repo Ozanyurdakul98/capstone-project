c6325d669b7a5b472278053db98dc98d09a008f5
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-11 17:19:46 +0100

    edit bg color

--- components/Homepage/HomepageStatsCounter.js
+++ components/Homepage/HomepageStatsCounter.js
@@ -1,7 +1,7 @@
 export function HomepageStatsCounter(props) {
   return (
     <section className='my-20'>
-      <div className=' flex flex-col rounded-xl border-2 border-b-4 border-b-[color:var(--primary-color)] bg-white py-2 shadow-inner'>
+      <div className=' flex flex-col rounded-xl border-2 border-b-4 border-b-[color:var(--primary-color)] bg-white/60 py-2 shadow-inner'>
         <h2 className=' label-form  text-center text-xl'>Statistics</h2>
         <div className='flex items-center justify-evenly '>
           <div className='relative flex h-40 w-36 flex-col items-center justify-center rounded-lg border border-[color:var(--primary-color-hover)] bg-white shadow-inner xl:w-48'>
