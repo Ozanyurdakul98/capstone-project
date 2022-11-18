89b31ea560485fe668dbe3a3b70c2eb84993ece7
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-18 18:53:09 +0100

    install react-table

--- components/Dashboard/DashboardStatsToday.js
+++ components/Dashboard/DashboardStatsToday.js
@@ -7,7 +7,7 @@ export default function DashboardBoxes() {
         <BsViewList className='h-8 w-8' />
         <div>
           <h2 className='text-2xl font-bold leading-4'>345</h2>
-          <h3 className='text-xs'>New Listings today</h3>
+          <h3 className='text-xs'>New Studios today</h3>
         </div>
       </div>
       <div className='text-primary flex w-1/2 grow items-center gap-3 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md bg-blue-300 p-5'>
