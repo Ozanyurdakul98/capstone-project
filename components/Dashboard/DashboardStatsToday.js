dc85e0fbdf1f0997332589a1edb3b63d0d7c4a02
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-18 17:07:55 +0100

    create dashboardStatsToday component

--- components/Dashboard/DashboardStatsToday.js
+++ components/Dashboard/DashboardStatsToday.js
@@ -1,42 +1,20 @@
+import { FaUsers } from 'react-icons/fa';
+import { BsViewList } from 'react-icons/bs';
 export default function DashboardBoxes() {
   return (
     <div className='flex gap-5'>
-      <div className='text-primary flex w-1/2 grow items-center gap-2 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md bg-blue-200 p-5'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='h-8 w-8'>
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z'
-          />
-        </svg>
+      <div className='text-primary flex w-1/2 grow items-center gap-3 rounded-tl-3xl rounded-br-3xl rounded-tr-md rounded-bl-md bg-blue-200 p-5'>
+        <BsViewList className='h-8 w-8' />
         <div>
-          <h2 className='text-2xl font-bold leading-4'>$345</h2>
-          <h3 className='text-xs'>Profit today</h3>
+          <h2 className='text-2xl font-bold leading-4'>345</h2>
+          <h3 className='text-xs'>New Listings today</h3>
         </div>
       </div>
-      <div className='text-primary flex w-1/2 grow items-center gap-2 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md bg-red-200 p-5'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='h-8 w-8'>
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z'
-          />
-        </svg>
+      <div className='text-primary flex w-1/2 grow items-center gap-3 rounded-tr-3xl rounded-bl-3xl rounded-tl-md rounded-br-md bg-blue-300 p-5'>
+        <FaUsers className='h-8 w-8' />
         <div>
           <h2 className='text-2xl font-bold leading-4'>21 392</h2>
-          <h3 className='text-xs'>Orders today</h3>
+          <h3 className='text-xs'>New Users today</h3>
         </div>
       </div>
     </div>
