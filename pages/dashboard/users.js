2681ea8dfe41bf44cd53d147a88e52d5c4549d2f
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-20 21:14:42 +0100

    add Users table page and navigation in dashboard

---
+++ pages/dashboard/users.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StudioTable from '../../components/Dashboard/Tables/StudioTable';
+import db from '../../lib/dbConnect';
+import StudioListing from '../../models/StudioListing';
+
+export default function DashboardStudios({ fetchedStudios }) {
+  return <StudioTable fetchedStudios={fetchedStudios} />;
+}
+
+export async function getServerSideProps(context) {
+  await db.connect();
+
+  const fetchingStudios = await StudioListing.find();
+  const fetchedStudios = JSON.parse(JSON.stringify(fetchingStudios));
+
+  return {
+    props: {
+      fetchedStudios: fetchedStudios || null,
+    },
+  };
+}
