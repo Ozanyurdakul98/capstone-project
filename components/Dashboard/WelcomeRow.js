a1bc8a82daa556848ef4647d784d6bf2b2ee6af6
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-11-22 18:09:02 +0100

    edit welcome row in dashboard to greet with according to daytime

--- components/Dashboard/WelcomeRow.js
+++ components/Dashboard/WelcomeRow.js
@@ -1,11 +1,46 @@
+import { format } from 'date-fns';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function WelcomeRow() {
+  const [greeting, setGreeting] = useState('');
+  useEffect(() => {
+    setGreeting(greetingUser());
+  }, []);
+
+  function greetingUser() {
+    const date = new Date();
+    const time = date.getHours();
+    if (time <= 1) {
+      return "it's midnight, did you know that";
+    } else if (time <= 3) {
+      return "It's middle in the night";
+    } else if (time <= 6) {
+      return "Good morning to you, it's early";
+    } else if (time <= 9) {
+      return 'Good morning';
+    } else if (time <= 11) {
+      return "It's late but, good morning";
+    } else if (time <= 12) {
+      return 'Good noon';
+    } else if (time <= 15) {
+      return 'Good afternoon';
+    } else if (time <= 16) {
+      return 'Good late afternoon';
+    } else if (time <= 20) {
+      return 'Good evening';
+    } else if (time <= 23) {
+      return 'Good evening for the night';
+    }
+  }
+
+  // format(date, 'dd/MM/yyyy');
+  // console.log(time);
   return (
     <div className='mb-6 flex items-center text-secondary-color'>
       <div className='w-1/2 grow'>
         <h1 className='text-xl'>
-          Welcome, <b>Dawid</b>
+          {greeting}, <b>Dawid</b>
         </h1>
       </div>
       <div className=''>
