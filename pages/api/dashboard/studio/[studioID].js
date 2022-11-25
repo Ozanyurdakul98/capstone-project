aa4458f64db1cfaa36a0ac9b472261dc44deaf10
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-11-25 19:53:56 +0100

    add api route protection for normal user routes

--- pages/api/dashboard/studio/[studioID].js
+++ pages/api/dashboard/studio/[studioID].js
@@ -1,35 +1,51 @@
-import db from '../../../../lib/dbConnect';
-import StudioListing from '../../../../models/StudioListing';
+import db from "../../../../lib/dbConnect";
+import StudioListing from "../../../../models/StudioListing";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(req, res) {
-  // const session = await unstable_getServerSession(req, res, authOptions);
-  //MAKE ACCESS ONLY TO ADMIN, ALLE METHODS USED
-  if (req.method === 'GET') {
+  const token = await getToken({ req });
+  if (token.role !== "user" && token.role !== "admin") {
+    return res.status(401).json({
+      success: false,
+      message:
+        "Your session is invalid. You are not authorized to do this action!",
+    });
+  }
+  if (req.method === "GET") {
     try {
       const { studioID } = req.query;
       const fetchingStudio = await StudioListing.find({ _id: studioID });
       return res.status(200).json({ success: true, data: fetchingStudio });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'Studio not found' });
+      return res
+        .status(400)
+        .json({ success: false, message: "Studio not found" });
     }
-  } else if (req.method === 'PATCH') {
+  } else if (req.method === "PATCH") {
     await db.connect();
     try {
       const { studioID } = req.query;
       const listing = await StudioListing.findByIdAndUpdate(studioID, req.body);
       return res.status(201).json({ success: true, data: listing });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'Unauthorized', error });
+      return res
+        .status(400)
+        .json({ success: false, message: "Unauthorized", error });
     }
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     await db.connect();
     try {
       const { studioID } = req.query;
       const status = await StudioListing.findByIdAndDelete(studioID);
       return res.status(201).json({ success: true, status });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'Unauthorized', error });
+      return res
+        .status(400)
+        .json({ success: false, message: "Unauthorized", error });
     }
   }
-  return res.status(400).json({ success: false, message: 'HTTP method is not allowed, Unauthorized' });
+  return res.status(400).json({
+    success: false,
+    message: "HTTP method is not allowed, Unauthorized",
+  });
 }
--- pages/api/dashboard/user/[userID].js
+++ pages/api/dashboard/user/[userID].js
@@ -1,35 +1,51 @@
-import db from '../../../../lib/dbConnect';
-import User from '../../../../models/UserModel';
+import db from "../../../../lib/dbConnect";
+import User from "../../../../models/UserModel";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(req, res) {
-  // const session = await unstable_getServerSession(req, res, authOptions);
-  //MAKE ACCESS ONLY TO ADMIN, ALLE METHODS USED
-  if (req.method === 'GET') {
+  const token = await getToken({ req });
+  if (token.role !== "user" && token.role !== "admin") {
+    return res.status(401).json({
+      success: false,
+      message:
+        "Your session is invalid. You are not authorized to do this action!",
+    });
+  }
+  if (req.method === "GET") {
     try {
       const { userID } = req.query;
       const fetchedUser = await User.find({ _id: userID });
       return res.status(200).json({ success: true, data: fetchedUser });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'User not found' });
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
     }
-  } else if (req.method === 'PATCH') {
+  } else if (req.method === "PATCH") {
     await db.connect();
     try {
       const { userID } = req.query;
       const user = await User.findByIdAndUpdate(userID, req.body);
       return res.status(201).json({ success: true, data: user });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'Unauthorized', error });
+      return res
+        .status(400)
+        .json({ success: false, message: "Unauthorized", error });
     }
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     await db.connect();
     try {
       const { userID } = req.query;
       const status = await User.findByIdAndDelete(userID);
       return res.status(201).json({ success: true, status });
     } catch (error) {
-      return res.status(400).json({ success: false, message: 'Unauthorized', error });
+      return res
+        .status(400)
+        .json({ success: false, message: "Unauthorized", error });
     }
   }
-  return res.status(400).json({ success: false, message: 'HTTP method is not allowed, Unauthorized' });
+  return res.status(400).json({
+    success: false,
+    message: "HTTP method is not allowed, Unauthorized",
+  });
 }
