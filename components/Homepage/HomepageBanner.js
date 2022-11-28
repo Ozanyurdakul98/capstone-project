af0e4948ecaf13f304f2b83853f0735108f1d975
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-11-28 22:04:57 +0100

    edit styling

--- components/Homepage/HomepageBanner.js
+++ components/Homepage/HomepageBanner.js
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export function HomepageBanner() {
   return (
     <section className='relative '>
-      <div className='relative grid h-screen w-full '>
+      <div className='relative grid min-h-screen  w-full '>
         <Image
           src='/images/Banner-homepage1.jpg'
           objectPosition='top'
@@ -14,15 +14,17 @@ export function HomepageBanner() {
         />
       </div>
       <div className='absolute top-1/2 left-1/2 w-80 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white/90 py-10 px-6 font-thin md:left-1/3 lg:left-1/3 lg:text-lg xl:text-xl'>
-        <h2 className='label-form text-sm'>Studio owners are benefiting</h2>
+        <h2 className='h3'>Studio owners are benefiting</h2>
         <p className='mb-4'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsa ipsam est quo atque quaerat veritatis
-          expedita adipisci soluta, ab veniam delectus sequi, ipsum accusantium explicabo non laudantium facere commodi.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsa ipsam est quo atque
+          quaerat veritatis expedita adipisci soluta, ab veniam delectus sequi, ipsum accusantium
+          explicabo non laudantium facere commodi.
         </p>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quas officiis accusamus corporis odio
-          voluptatem quidem iure earum, cupiditate voluptates consequatur. Vero architecto amet provident delectus,
-          similique consequuntur harum blanditiis incidunt distinctio libero omnis ullam sapiente cumque.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quas officiis accusamus
+          corporis odio voluptatem quidem iure earum, cupiditate voluptates consequatur. Vero
+          architecto amet provident delectus, similique consequuntur harum blanditiis incidunt
+          distinctio libero omnis ullam sapiente cumque.
         </p>
         <Link href='/signup'>
           <button className=' button mt-4 text-base font-semibold'>Become a Host</button>
--- components/Homepage/HomepageStatsCounter.js
+++ components/Homepage/HomepageStatsCounter.js
@@ -1,14 +1,13 @@
 export function HomepageStatsCounter(props) {
   return (
     <section className='my-20'>
-      <div className=' flex flex-col rounded-xl border-2 border-b-4 border-b-[color:var(--primary-color)] bg-white/60 py-2 shadow-inner'>
-        <h2 className=' label-form  text-center text-xl'>Statistics</h2>
+      <div className=' flex flex-col rounded-xl border-2 border-b-4 border-b-[color:var(--primary-color)]   py-2  '>
         <div className='flex items-center justify-evenly '>
-          <div className='relative flex h-40 w-36 flex-col items-center justify-center rounded-lg border border-[color:var(--primary-color-hover)] bg-white shadow-inner xl:w-48'>
+          <div className='relative flex h-40 w-36 flex-col items-center justify-center rounded-xl border border-[color:var(--primary-color-hover)] bg-white shadow-inner xl:w-48'>
             <span className=' text-5xl'>{props.totalListings}</span>
             <h3 className=' absolute bottom-2 text-lg font-normal'>Total Listings</h3>
           </div>
-          <div className='relative flex h-40 w-36 flex-col items-center justify-center rounded-lg border border-[color:var(--primary-color-hover)] bg-white shadow-inner xl:w-48'>
+          <div className='relative flex h-40 w-36 flex-col items-center justify-center rounded-xl border border-[color:var(--primary-color-hover)] bg-white shadow-inner xl:w-48'>
             <span className=' text-5xl'>{props.totalUsers}</span>
             <h3 className=' absolute bottom-2 text-lg font-normal'>Total Users</h3>
           </div>
