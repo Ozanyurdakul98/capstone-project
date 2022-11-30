fe477927a11f9601d2885e39225458d4ed810606
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-11-30 17:42:59 +0100

    add header for grid studio services section

--- pages/index.js
+++ pages/index.js
@@ -19,32 +19,37 @@ export default function Home({ latestListings, totalUsers, totalListings, studio
       </Head>
       <HomepageHero />
       <HomepageStatsCounter totalUsers={totalUsers} totalListings={totalListings}></HomepageStatsCounter>
-      <section className="mx-2 grid grid-cols-2 gap-5 md:grid-cols-3">
-        {studioServices.map((service) => (
-          <div key={service.id} className="flex justify-center">
-            <div className="max-w-sm rounded-lg bg-white shadow-lg">
-              <a href="#!">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img className="rounded-t-lg" src={service.image} alt="" />
-              </a>
-              <div className="p-6">
-                <h5 className=" h3 mb-2 text-sm font-medium md:text-lg">{service.name}</h5>
-                <p className=" mb-4 text-xs text-gray-700 line-clamp-3 md:text-base">{service.description}</p>
-                <button
-                  type="button"
-                  onClick={() =>
-                    router.push({
-                      pathname: `/studioservice/${service.queryString}`,
-                    })
-                  }
-                  className=" bg-primary inline-block rounded-xl px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg">
-                  Show me
-                </button>
+      <div className=" flex flex-col gap-10 px-2">
+        <div className="px-5">
+          <h2 className="h2">Studio services</h2>
+        </div>
+        <section className="grid grid-cols-2 gap-5 md:grid-cols-3">
+          {studioServices.map((service) => (
+            <article key={service.id} className="flex justify-center">
+              <div className="max-w-sm rounded-lg bg-white shadow-lg">
+                <a href="#!">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img className="rounded-t-lg" src={service.image} alt="" />
+                </a>
+                <div className="p-6">
+                  <h5 className=" h3 mb-2 text-sm font-medium md:text-lg">{service.name}</h5>
+                  <p className=" mb-4 text-xs text-gray-700 line-clamp-3 md:text-base">{service.description}</p>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      router.push({
+                        pathname: `/studioservice/${service.queryString}`,
+                      })
+                    }
+                    className=" bg-primary inline-block rounded-xl px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg">
+                    Show me
+                  </button>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </section>
+            </article>
+          ))}
+        </section>
+      </div>
       <Latest10Listings latestListings={latestListings} />
       <HomepageBanner />
     </div>
@@ -62,7 +67,6 @@ export async function getStaticProps() {
   const latestAddedListings = await StudioListing.find().sort({ $natural: -1 }).limit(10).populate('studioService');
   const serializedLatestAddedListings = JSON.parse(JSON.stringify(latestAddedListings));
   const services = await StudioService.find();
-
   const sanitizedServices = services.map((service) => ({
     id: service.id,
     image: service.image,
