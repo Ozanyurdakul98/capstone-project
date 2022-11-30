02ca5c31bda775517655d5d63a9a7873ec3f6fb9
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-11-30 22:47:58 +0100

    add dynamic routing for studioTypes

---
+++ pages/studiotype/[type].js
@@ -0,0 +1,51 @@
+//db
+import db from '../../lib/dbConnect';
+import StudioListing from '../../models/StudioListing';
+//components
+import Layout from '../../components/Layout/Layout';
+import { Resultpage } from '../../components/Result/Resultpage';
+
+function StudioTypeResults({ studios, studioType }) {
+  return <Resultpage studios={studios} header={studioType}></Resultpage>;
+}
+
+export default StudioTypeResults;
+
+StudioTypeResults.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
+
+export async function getServerSideProps(context) {
+  await db.connect();
+  const { type } = context.query;
+  console.log(type, 'type');
+  let sanitizedStudios;
+  let studioType;
+  if (type === 'homestudio') {
+    studioType = 'Home Studio';
+  } else if (type === 'mediumstudio') {
+    studioType = 'Medium Studio';
+  } else if (type === 'premiumstudio') {
+    studioType = 'Premium Studio';
+  }
+  const getStudiosWithType = await StudioListing.find({ studiotype: 'Home Studio' })
+    .populate({
+      path: 'studioService',
+      model: 'StudioService',
+      select: 'name -_id',
+    })
+    .sort({ $natural: -1 });
+  const serializing = JSON.parse(JSON.stringify(getStudiosWithType));
+  sanitizedStudios = serializing.map((studio) => ({
+    ...studio,
+    studioService: studio.studioService.map((service) => service.name),
+  }));
+
+  console.log('sanitized', sanitizedStudios);
+  return {
+    props: {
+      studios: sanitizedStudios || null,
+      studioType: studioType || null,
+    },
+  };
+}
