8eaa90d1d3caa64017607b4f278cd3a33d5af83d
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-11-30 22:03:41 +0100

    edit homepage banner to be more responsive // edit its design

--- components/Homepage/HomepageBanner.js
+++ components/Homepage/HomepageBanner.js
@@ -3,17 +3,20 @@ import Link from 'next/link';
 
 export function HomepageBanner() {
   return (
-    <section className="mb-40 grid h-[750px] grid-cols-2">
-      <div className="relative h-full w-full ">
-        <Image
-          src="/images/Banner-homepage1.jpg"
-          objectPosition="top"
-          layout="fill"
-          objectFit="contain"
-          alt="login-image"
-        />
+    <section className="relative mb-40 grid h-[550px] grid-cols-2 lg:h-[750px] lg:gap-20">
+      <div className="flex justify-end md:w-full">
+        <div className="h-full w-full sm:relative md:w-80 lg:w-[500px]">
+          <Image
+            className="rounded-xl sm:rounded-r-none lg:rounded-r-xl"
+            src="/images/Banner-homepage1.jpg"
+            objectPosition="top"
+            layout="fill"
+            objectFit="cover"
+            alt="login-image"
+          />
+        </div>
       </div>
-      <div className="w-80 rounded-lg bg-white/90 py-10 px-6 font-thin lg:text-lg xl:text-xl">
+      <div className="absolute top-1/2 left-1/2 w-80  -translate-x-1/2 -translate-y-1/2 rounded-xl bg-white/90 py-10 px-6 font-thin sm:static sm:transform-none sm:rounded-l-none lg:rounded-l-xl lg:text-lg xl:text-xl">
         <h2 className="h3">Studio owners are benefiting</h2>
         <p className="mb-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsa ipsam est quo atque quaerat veritatis
