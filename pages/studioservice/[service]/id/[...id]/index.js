a6365769381ae7f0724bd9abc90ec7d481570f07
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-12-02 02:41:46 +0100

    add detailpage details section final styling

--- pages/studioservice/[service]/id/[...id]/index.js
+++ pages/studioservice/[service]/id/[...id]/index.js
@@ -171,7 +171,7 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
           </section>
           {/* Details */}
           <section className="px-7 text-xs text-gray-600">
-            <ul className="grid sm:grid-cols-smbgbg sm:grid-rows-4">
+            <ul className="grid grid-cols-smbg grid-rows-6 gap-2 sm:grid-cols-smbgbg sm:grid-rows-4">
               <li className="h2LandingP col-start-1 text-sm font-bold">Details</li>
               <li className="col-start-2 row-start-1 flex items-center">
                 <svg
@@ -198,6 +198,58 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
                     clipRule="evenodd"></path>
                 </svg>
                 Capacity: <span className="pl-1 font-semibold">{studio.maxGuests + ' Guests max'}</span>
+              </li>
+              <li className="col-start-2 row-start-3 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Beds: <span className="pl-1 font-semibold">No Sleepover</span>
+              </li>
+              <li className="col-start-2 row-start-4 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Studio rooms: <span className="pl-1 font-semibold">3</span>
+              </li>
+              <li className="col-start-2 row-start-5 flex items-center sm:col-start-3 sm:row-start-1">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Studio type: <span className="pl-1 font-semibold">{studio.studiotype}</span>
+              </li>
+              <li className="col-start-2 row-start-6 flex items-center sm:col-start-3 sm:row-start-2">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Property size: <span className="pl-1 font-semibold">78 sqm</span>
               </li>
             </ul>
           </section>
