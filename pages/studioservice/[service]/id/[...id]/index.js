467598ba40b69bde5ff57f5f8902ef7e73b10547
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-12-03 19:27:17 +0100

    edit details section

--- pages/studioservice/[service]/id/[...id]/index.js
+++ pages/studioservice/[service]/id/[...id]/index.js
@@ -105,7 +105,29 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
               </ol>
             </nav>
             <div className="flex items-center justify-between gap-5 sm:pr-7">
-              <h1 className="h1LandingP">{studio.listingTitle}</h1>
+              <div className="flex flex-col gap-1">
+                <h1 className="h1LandingP">{studio.listingTitle}</h1>
+                <h3 className="flex items-center gap-2 text-gray-500">
+                  <svg
+                    className="h-5 w-5 text-black"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                  </svg>
+                  {studio.studioLocation}
+                </h3>
+              </div>
               <div className="relative flex h-14 w-14 shrink-0 md:h-16 md:w-16 xl:h-20 xl:w-20">
                 <Image
                   src={studio.user.avatar}
@@ -117,26 +139,6 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
                 />
               </div>
             </div>
-            <h3 className="flex items-center gap-2 text-gray-500">
-              <svg
-                className="h-5 w-5 text-black"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              </svg>
-              {studio.studioLocation}
-            </h3>
           </section>
           {/* FeatureSection */}
           <section className=" mb-10 text-sm text-gray-600">
@@ -183,9 +185,131 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
             </div>
           </section>
           {/* Details */}
-          <section className="mb-10 px-7 text-xs text-gray-600">
+          <section className="mb-7 px-7 text-xs text-gray-600 lg:text-sm">
             <ul className="grid grid-cols-smbg grid-rows-6 gap-2 sm:grid-cols-smbgbg sm:grid-rows-4">
-              <li className="h2LandingP col-start-1 text-sm font-bold">Details</li>
+              <li className="h2LandingP col-start-1 row-span-2 text-sm font-bold lg:text-base">Details</li>
+              <li className="col-start-2 row-start-1 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                ID: <span className=" pl-1 font-semibold">{studio._id}</span>
+              </li>
+              <li className="col-start-2 row-start-2 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Capacity: <span className="pl-1 font-semibold">{studio.maxGuests + ' Guests max'}</span>
+              </li>
+              <li className="col-start-2 row-start-3 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Beds: <span className="pl-1 font-semibold">No Sleepover</span>
+              </li>
+              <li className="col-start-2 row-start-4 flex items-center">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Studio rooms: <span className="pl-1 font-semibold">3</span>
+              </li>
+              <li className="col-start-2 row-start-5 flex items-center sm:col-start-3 sm:row-start-1">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Studio size: <span className="pl-1 font-semibold">78 sqm</span>
+              </li>
+              <li className="col-start-2 row-start-6 flex items-center sm:col-start-3 sm:row-start-2">
+                <svg
+                  className="mr-1 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"></path>
+                </svg>
+                Studio type: <span className="pl-1 font-semibold">{studio.studiotype}</span>
+              </li>
+            </ul>
+          </section>
+          {/* Studioservices */}
+          <section className="mb-10 px-7 text-xs text-gray-600 lg:text-sm">
+            <ul className="grid grid-cols-smbg grid-rows-1 gap-2 sm:grid-cols-smbgbg sm:grid-rows-1">
+              {/* <li className="h2LandingP col-start-1 row-span-2 text-sm font-bold lg:text-base">Studio services</li> */}
+              <li className="col-start-2 row-start-1 flex items-start sm:col-span-2 sm:col-start-2 sm:row-start-1">
+                <div className="flex items-center">
+                  <svg
+                    className="mr-1 h-4 w-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"></path>
+                  </svg>
+                  <span className="whitespace-nowrap">Studio services:</span>
+                </div>
+                <span className="pl-1 text-[14px] font-semibold">{studio.studioService.join(', ')}</span>
+                {/* <span className="pl-1 font-semibold">{studio.studioService.map((service) => service + ', ')}</span> */}
+              </li>
+            </ul>
+          </section>
+          {/* ImageSection */}
+          <section className="mb-10">
+            <div className="studioImgsLP relative h-80 w-full md:h-[450px]">
+              <Image src={wordData.value} alt="" layout="fill" objectFit="cover" />
+              {/* <Image src={wordData.value} alt="" height="300" width="500" /> */}
+            </div>
+            <div className="flex_row">
+              {imgs.map((data, i) => (
+                <div className={`thumbnail relative h-[70px] w-[100px]`} onClick={() => handleClick(i)} key={i}>
+                  <Image src={data.value} layout="fill" objectFit="cover" alt="studio image" />
+                  <div className={`absolute h-full w-full bg-black/50  ${wordData.id == i ? 'hidden' : ''}`}></div>
+                </div>
+              ))}
+            </div>
+          </section>
+          {/* PricingSection */}
+          <section className="mb-10 px-7 text-xs text-gray-600 lg:text-sm">
+            <ul className="grid grid-cols-smbg grid-rows-6 gap-2 sm:grid-cols-smbgbg sm:grid-rows-4">
+              <li className="h2LandingP col-start-1 text-sm font-bold lg:text-base">Details</li>
               <li className="col-start-2 row-start-1 flex items-center">
                 <svg
                   className="mr-1 h-4 w-4"
@@ -265,21 +389,6 @@ function StudioDetailpage({ serializedStudio, breadCrumb }) {
                 Property size: <span className="pl-1 font-semibold">78 sqm</span>
               </li>
             </ul>
-          </section>
-          {/* ImageSection */}
-          <section className="mb-10">
-            <div className="studioImgsLP relative h-80 w-full md:h-[450px]">
-              <Image src={wordData.value} alt="" layout="fill" objectFit="cover" />
-              {/* <Image src={wordData.value} alt="" height="300" width="500" /> */}
-            </div>
-            <div className="flex_row">
-              {imgs.map((data, i) => (
-                <div className={`thumbnail relative h-[70px] w-[100px]`} onClick={() => handleClick(i)} key={i}>
-                  <Image src={data.value} layout="fill" objectFit="cover" alt="studio image" />
-                  <div className={`absolute h-full w-full bg-black/50  ${wordData.id == i ? 'hidden' : ''}`}></div>
-                </div>
-              ))}
-            </div>
           </section>
         </section>
         {/* SideTable */}
