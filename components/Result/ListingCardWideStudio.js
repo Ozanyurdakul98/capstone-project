15f5a6c22e439b3fcf6eb571a492f5ee72aa3c8a
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-12-17 15:36:46 +0100

    edit studioListingCard style

--- components/Result/ListingCardWideStudio.js
+++ components/Result/ListingCardWideStudio.js
@@ -35,17 +35,25 @@ function ListingCardWideStudio({
             }
       }>
       <article>
-        <div className="flex w-full cursor-pointer items-center rounded-lg border-b py-7 px-2 first:border hover:opacity-80 hover:shadow-lg">
+        <div className="flex w-full cursor-pointer items-center rounded-lg border-b bg-white p-2 first:border hover:opacity-80 hover:shadow-lg">
+          {/* image */}
           <div className="flex h-24 w-32 shrink-0 justify-center sm:h-32 sm:w-48 md:h-36 md:w-56 lg:h-52 lg:w-80">
             <div className="relative h-24 w-24  shrink-0 sm:h-32 sm:w-32 md:h-36 md:w-36 lg:h-52 lg:w-52">
               <Image src={logo} layout="fill" objectFit="cover" className="rounded-full" alt="Thumbnail" />
             </div>
           </div>
+          {/* informations */}
           <div className="flex w-full flex-col justify-between pl-2 sm:pl-5">
             <div className="flex flex-col md:gap-2">
               {/* studiolocation */}
-              <div className="flex items-center justify-between">
+              <div className="relative flex items-center justify-between">
                 <p className="truncate text-xs text-gray-400">{studioLocation}</p>
+                <div className="absolute right-0 top-0 flex flex-col items-end truncate text-xs text-gray-400">
+                  <div className="relative h-10 w-10 shrink-0 md:h-36 md:w-36 lg:h-52 lg:w-52">
+                    <Image src={user.avatar} layout="fill" objectFit="cover" className="rounded-full" alt="Thumbnail" />
+                  </div>
+                  <p className="truncate text-xs text-gray-400">{'uiwnd uawndiuwadiaw feefe'}</p>
+                </div>
               </div>
               {/* studiotype */}
               <div className="flex gap-2">
@@ -54,14 +62,14 @@ function ListingCardWideStudio({
                 </p>
                 <p
                   className={
-                    'bg-primary truncate  rounded border border-slate-700 px-1  text-xs text-white sm:text-sm md:text-sm  '
+                    'bg-primary truncate  rounded border border-slate-700 px-1 text-xs text-white sm:text-sm md:text-sm  '
                   }>
                   {openingHours}
                 </p>
               </div>
             </div>
             {/* StudioName */}
-            <h4 className="pt-1 text-[16px] sm:text-xl md:text-2xl">{studioName}</h4>
+            <h4 className="pt-1 text-[16px] sm:text-xl lg:text-2xl">{studioName}</h4>
             <div className="flex   items-center  ">
               <p className=" pr-1 text-sm line-clamp-1 sm:text-sm md:text-sm lg:text-base xl:text-lg">
                 {studioLanguages.join('  ')}
