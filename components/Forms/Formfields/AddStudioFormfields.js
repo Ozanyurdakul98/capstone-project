e65191cdc055b72d18a34662f995fa9be50b0197
Ozan Yurdakul <68496958+Ozanyurdakul98@users.noreply.github.com>
2022-12-17 16:10:56 +0100

    move formfields to folder

--- components/Forms/Formfields/AddStudioFormfields.js
+++ components/Forms/Formfields/AddStudioFormfields.js
@@ -1,9 +1,9 @@
-import { FormInput } from './FormInput';
+import { FormInput } from '../FormInput';
 import Image from 'next/image.js';
 import { TbHandClick } from 'react-icons/tb';
 import { MdDeleteForever } from 'react-icons/md';
 
-export function AddStudioForm(props) {
+export function AddStudioFormfields(props) {
   return (
     <>
       {/* Logo, studioname-/text */}
